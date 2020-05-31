5c73e69b140feac9fdfbfe970438f11d53b8bb46
user42 <sablayrolles.antoine@gmail.com>
2020-05-31 15:50:55 +0200

    minikube start

--- ft_service/srcs/setup.sh
+++ ft_service/srcs/setup.sh
@@ -1,14 +1,15 @@
-minikube start ;
+minikube start --driver=docker;
 eval $(minikube docker-env);
-docker build .
-kubectl create deployment Nginx-node --image="Nginx-deployment";
+docker build -t ngnix:1.0 ./srcs/nginx ;
+kubectl apply -f srcs/nginx/deployment.yaml;
 kubectl expose deployment/Nginx-node --type="NodePort" --port 80 --port 443;
 export POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}');
 export NODE_PORT=$(kubectl get services/Nginx-node -o go-template='{{(index .spec.ports 0).nodePort}}')
-kubectl create deployment WordPress-node --image= ;
-kubectl create deployment MySQL-node --image= ;
-kubectl create deployment phpmyadmin-node --image= ;
-kubectl create deployment FTPS-node --image= ;
+kubectl apply -f srcs/wordpress/deployment.yaml;
+kubectl apply -f srcs/mysql/deployment.yaml;
+kubectl apply -f srcs/phpmyadmin/deployment.yaml;
+docker build -t ftps:1.0 ./srcs/ftp ;
+kubectl apply -f srcs/ftp/deployment.yaml;
 kubectl expose deployment/FTPS-node --type="NodePort" --port 21;
 kubectl create deployment ingress_controller-node --image= ;
 kubectl create deployment graphana-node --image= ;
